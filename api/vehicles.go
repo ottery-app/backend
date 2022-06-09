1d41bc8690edf4796f2cebe21c5245f66d950dba
lewibs <benjaminsl2000@gmail.com>
2022-06-08 20:29:03 -0400

    added api for user and vehicle

--- api/vehicles.go
+++ api/vehicles.go
@@ -125,5 +125,95 @@ func Vehicles(router *gin.Engine, mon mon.Mon) *gin.Engine {
 		})
 	})
 
+	router.PUT("vehicles/:id", func(c *gin.Context) {
+		var vehicle types.Vehicle
+		c.Bind(&vehicle)
+
+		token := c.GetHeader("Authorization")
+		if token == "" {
+			HandleError(c, http.StatusUnauthorized, fmt.Errorf("no token provided"))
+			return
+		}
+
+		userSesh := sesh.GetSesh()[token]
+		if userSesh.Username == "" {
+			HandleError(c, http.StatusUnauthorized, fmt.Errorf("no user found"))
+			return
+		}
+
+		//get the user from the database and check if the user has the vehicle
+		user, err := mon.GoUser.Get(userSesh.Username)
+		if err != nil {
+			HandleError(c, http.StatusInternalServerError, err)
+			return
+		}
+
+		//check that the vehicle is in the user's vehicles
+		vehicleId := c.Param("id")
+
+		//check to see if the user has the vehicle
+		if !user.ContainsVehicle(vehicleId) {
+			HandleError(c, http.StatusUnauthorized, fmt.Errorf("user does not have vehicle"))
+			return
+		}
+
+		//update the vehicle in the database
+		err = mon.GoVehicle.Update(vehicleId, vehicle)
+		if err != nil {
+			HandleError(c, http.StatusInternalServerError, err)
+			return
+		}
+
+		HandleSuccess(c, http.StatusAccepted, gin.H{
+			"message": "vehicle updated",
+		})
+	})
+
+	router.DELETE("vehicles/:id", func(c *gin.Context) {
+		token := c.GetHeader("Authorization")
+		if token == "" {
+			HandleError(c, http.StatusUnauthorized, fmt.Errorf("no token provided"))
+			return
+		}
+
+		userSesh := sesh.GetSesh()[token]
+
+		if userSesh.Username == "" {
+			HandleError(c, http.StatusUnauthorized, fmt.Errorf("no user found"))
+			return
+		}
+
+		//get the user from the database and check if the user has the vehicle
+		user, err := mon.GoUser.Get(userSesh.Username)
+		if err != nil {
+			HandleError(c, http.StatusInternalServerError, err)
+			return
+		}
+
+		//check that the vehicle is in the user's vehicles
+		//if it is remove it from the user's vehicles and also the mongo database
+		vehicleId := c.Param("id")
+
+		//check to see if the user has the vehicle
+		if !user.ContainsVehicle(vehicleId) {
+			HandleError(c, http.StatusUnauthorized, fmt.Errorf("user does not have vehicle"))
+			return
+		}
+
+		//remove the vehicle from the user's vehicles
+		mon.GoUser.RemoveFromField(userSesh.Username, "vehicles", vehicleId)
+
+		//remove the vehicle from the database
+		err = mon.GoVehicle.Delete(vehicleId)
+		if err != nil {
+			HandleError(c, http.StatusInternalServerError, err)
+			return
+		}
+
+		HandleSuccess(c, http.StatusAccepted, gin.H{
+			"message": "vehicle deleted",
+		})
+	})
+
 	return router
 }
