c0e59b733428efc36636d7037d1aff06d5951af0
lewibs <benjaminsl2000@gmail.com>
2023-12-05 00:26:42 -0500

    findihed #170

--- src/functions/normalizeId.ts
+++ src/functions/normalizeId.ts
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
-import { id } from "@ottery/ottery-dto";
+import { id, noId } from "@ottery/ottery-dto";
 
 export function normalizeId(id: mongoose.Types.ObjectId | id) {
+    if (id === noId) {
+        id = "000000000000000000000000";
+    }
+    
     return new mongoose.Types.ObjectId(id);
 }
