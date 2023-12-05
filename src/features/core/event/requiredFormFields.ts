bbf35ccc946a1ca49e0d3a58e963ad4f294e5dce
lewibs <benjaminsl2000@gmail.com>
2023-12-04 21:01:03 -0500

    added roster getter

--- src/features/core/event/requiredFormFields.ts
+++ src/features/core/event/requiredFormFields.ts
@@ -1,18 +1,18 @@
-import { CustomFormFieldDto, classifyDto } from "@ottery/ottery-dto";
+import { FormFieldDto, classifyDto } from "@ottery/ottery-dto";
 import { inputType } from "@ottery/ottery-dto/lib/types/input/input.enums";
 
-const PFP = new CustomFormFieldDto();
+const PFP = new FormFieldDto();
 PFP.label = "profile picture";
 PFP.note = "This photo is required for security purposes",
-PFP.optional = false;
-PFP.permanent = true;
+// PFP.optional = false;
+// PFP.permanent = true;
 PFP.type = inputType.PICTURE;
 
-const EMERGENCY_CONTACT = new CustomFormFieldDto();
+const EMERGENCY_CONTACT = new FormFieldDto();
 EMERGENCY_CONTACT.label = "emergency contact",
 EMERGENCY_CONTACT.note = "Who should be called in case of emergency",
-EMERGENCY_CONTACT.optional = false;
-EMERGENCY_CONTACT.permanent = true;
+// EMERGENCY_CONTACT.optional = false;
+// EMERGENCY_CONTACT.permanent = true;
 EMERGENCY_CONTACT.type = inputType.PHONE;
 
 export const VOLENTEER_REQUIRED = [
--- src/features/roster/roster.controller.ts
+++ src/features/roster/roster.controller.ts
@@ -1,17 +1,45 @@
-import { Controller, Body, Patch, Param, HttpException, HttpStatus } from '@nestjs/common';
-import { IdArrayDto, id, noId } from '@ottery/ottery-dto';
+import { Controller, Body, Patch, Get, Param, Query, HttpException, HttpStatus } from '@nestjs/common';
+import { IdArrayDto, id, noId, tempzone } from '@ottery/ottery-dto';
 import { Sesh } from '../auth/sesh/Sesh.decorator';
 import { LocatableService } from '../locatable/locatable.service';
-import { RosterService } from './roster.service';
 import { CoreService } from '../core/core.service';
+import { normalizeId } from 'src/functions/normalizeId';
 
 @Controller('api/roster')
 export class RosterController {
   constructor(
     private locatableService: LocatableService,
-    private rosterService: RosterService,
     private coreService: CoreService,
   ) {}
+
+  @Get(":eventId/attendees")
+  async getAttendees(
+    @Param("eventId") eventId: id,
+    @Query("present") present: boolean | undefined,
+  ) {
+    present = present === 'true' as unknown as boolean
+    const event = await this.coreService.event.get(eventId);
+    const attendees = (await this.coreService.child.getMany(event.attendees)).filter((child)=>{
+      
+      if (present === true) {
+        if (child.lastStampedLocation?.at) {
+          return normalizeId(child.lastStampedLocation.at).equals(normalizeId(event._id));
+        } else {
+          return false;
+        }
+      } else if (present === false) {
+        if (child.lastStampedLocation?.at) {
+          return !normalizeId(child.lastStampedLocation.at).equals(normalizeId(event._id));
+        } else {
+          return true;
+        }
+      }
+
+      return true;
+    });
+
+    return attendees;
+  }
 
   @Patch(":eventId/dropOff")
   async dropOff(
@@ -26,13 +54,14 @@ export class RosterController {
       )
     }
 
-    // const event = await this.coreService.event.get(eventId);
+    const event = await this.coreService.event.get(eventId);
+    const children = await this.coreService.child.getMany(childIds.ids);
 
-    // const children = await this.coreService.child.getMany(childIds.ids);
-
-
-    console.log(sesh);
-    console.log(eventId);
-    console.log(childIds);
+    if (event.tempzone === tempzone.Default) {
+      return await Promise.all(children.map(async (child)=>{
+        this.locatableService.stamp(child, event._id, noId);
+        return await child.save();
+      }));
+    }
   }
 }
